08557cfd4e82e7df5fc8b5cc65397685231c73a2
Jason Sydes <sydes@talapas-ln1.cm.cluster>
2019-08-24 12:53:06 -0700

    small setup change

--- setup.sh
+++ setup.sh
@@ -2,6 +2,10 @@
 
 cd $HOME
 
+# vim
 ln -s .dotfiles/vim/vimrc .vimrc
+# tmux + tmux plugin manager
 ln -s .dotfiles/tmux/tmux.conf .tmux.conf
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# Alacritty teriminal
 ln -s .dotfiles/terminal/alacritty.yml .alacritty.yml
