341a2630bd42027351730ba6832bc7fb239f9816
Jason Sydes <sydes@uoregon.edu>
2019-08-25 23:11:41 -0700

    various

--- setup.sh
+++ setup.sh
@@ -3,9 +3,11 @@
 cd $HOME
 
 # vim
-ln -s .dotfiles/vim/vimrc .vimrc
+ln -s .dotfiles/vim/vimrc ~/.vimrc
 # tmux + tmux plugin manager
-ln -s .dotfiles/tmux/tmux.conf .tmux.conf
+ln -s .dotfiles/tmux/tmux.conf ~/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # Alacritty teriminal
-ln -s .dotfiles/terminal/alacritty.yml .alacritty.yml
+ln -s .dotfiles/terminal/alacritty.yml ~/.alacritty.yml
+# inputrc
+ln -s .dotfiles/inputrc ~/.inputrc
