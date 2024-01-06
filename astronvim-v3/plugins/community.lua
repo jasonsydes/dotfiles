778858e85f53707385affa720247a1940ecc0785
Jason Sydes <sydes@uoregon.edu>
2024-01-06 11:32:29 -0800

    Some updates to astronvim:
    - added Black
    - added Harpoon
    - Some documentation updates (e.g., use astrocommunity)

--- astronvim-v3/plugins/community.lua
+++ astronvim-v3/plugins/community.lua
@@ -1,9 +1,49 @@
 return {
   -- Add the community repository of plugin specifications
+  -- Jason Note: This next line makes ALL of the AstroNvim community plugin *specifications*
+  --             available to you to use here. Find a list of the community plugins here:
+  --             https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity
   "AstroNvim/astrocommunity",
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  --
+  -- Jason Note: If you want to further configure the community plugin, follow this
+  --             guide on the README page here:
+  --             https://github.com/AstroNvim/astrocommunity#userpluginscommunitylua-example 
+
+  -- Harpoon
+  --    Usage:
+  --        https://github.com/AstroNvim/astrocommunity/blob/main/lua/astrocommunity/motion/harpoon/init.lua
+  --        https://github.com/ThePrimeagen/harpoon
+  --        Basic Examples:
+  --            <leader><leader>        - Show the help menu
+  --            <leader><leader>a       - Toggle the quick menu (this is the main interface; use it to jump around)
+  --            <leader><leader>e       - Toggle the quick menu (this is the main interface; use it to jump around)
+  --                j and k to move around within the quick menu (like other telescope interfaces) 
+  --                <enter> to jump to the selected file.
+  --            In Normal mode (huh, this community plugin is pretty opinionated to bind these keys):
+  --            <C-x>                   - Harpoon mark index (I think you enter integers here, and it jumps you) 
+  --            <C-p>                   - Goto previous (harpoon?) mark
+  --            <C-n>                   - Goto next (harpoon?) mark
+  --                          
+  { import = "astrocommunity.motion.harpoon" },
+
+  -- ------------------------------------------------------------
+  -- JUST TRYING TO GET BLACK TO WORK EASILY HERE...
+  -- Didn't work here. 
+  -- But I DID get it to work over in plugins/user.lus. Go there.
+  --
+  -- Python Pack 
+  --    I'm trying this out for the "Black" formatter. 
+  -- { import = "astrocommunity.pack.python" },
+  -- didn't seem to work...
+  --
+  -- python-ruff pack
+  --    I'm trying this out for the "Black" formatter. 
+  -- { import = "astrocommunity.pack.python-ruff" },
+  -- gave up trying to make this work.
+  -- ------------------------------------------------------------
+
 }
--- astronvim-v3/plugins/user.lua
+++ astronvim-v3/plugins/user.lua
@@ -22,8 +22,52 @@ return {
     lazy = false,
   },
   -- Doesn't seem to work?
-  -- { 'karb94/neoscroll.nvim', lazy = false}
+  -- { 'karb94/neoscroll.nvim', lazy = false},
+
+  -- cue syntax highlighting, maybe. (seems to work?)
+  -- { 'jjo/vim-cue', lazy = false},
+  { 'jjo/vim-cue', lazy = true },
+  -- -- cue treesitter grammer, maybe
+  -- { 'nvim-treesitter/nvim-treesitter', lazy = false},
+
+  -- --------------------------------------------------------------------------------------------------------
+  -- Let's try Black
+  --    Install:
+  --        # Install the python neovim client (https://github.com/neovim/pynvim) like so:
+  --        conda install neovim
+  --        # I *think* I had to install Black on the command line as well?
+  --        conda install black
+  --
+  --    Usage:
+  --        :Black (takes a moment or two...)
+  --    Note:
+  --        20240106
+  --        I tried a BUNCH of different ways to make Black work with AstroNvim.
+  --        There probably is some perfect way, I just wasn't able to uncover it 
+  --        in about oh say 30 minutes of digging.
+  --
+  --        Here's a few of the ways I tried (that didn't work):
+  --            - Mason installed the 'black' and 'blackd-client' (these show up as "Formatters")
+  --                - This causes 'black' to show up at bottom right corner, but doesn't seem to do anything.
+  --            - Added the "astrocommunity.pack.python" pack to community.lua
+  --                - Why? Because it had "black" in it.
+  --                - Didn't seem to work.
+  --            - Someone suggested the ruff-lsp
+  --                - Didn't seem to work (can't type ":Black", can't find neovim docs)
+  --            - Tried all of the above after doing conda install black.
+  --            - Probably other approached I'm forgetting about. 
+  --        
+  --        Here's what I tried that DID work:
+  --            - I installed the official psf/black plugin (see below).
+  --                - This is DISTINCT from all the other approaches above.
+  --                - It worked!
+  --    Usage:
+  --        Type ":Black" and wait a few seconds.
+  { 'psf/black',
+    ft = "python", -- NOTE: ft: lazy-load on filetype
+  },
+  -- --------------------------------------------------------------------------------------------------------
 
   -- Disable these plugins that AstroNvim includes by default.
-  {'goolord/alpha-nvim', enabled = false}
+  {'goolord/alpha-nvim', enabled = false },
 }
