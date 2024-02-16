64cdef88068750e8937ed74eb805dc4d1452fc3b
Jason Sydes <sydes@uoregon.edu>
2024-02-16 15:09:00 -0800

    Updated for is-svc-racs-gc3f GC3F Talapas role user.

--- setup.sh
+++ setup.sh
@@ -19,28 +19,41 @@
 
 cd $HOME
 
-# bash
-ln -s .dotfiles/bash/bash_profile ~/.bash_profile
-ln -s .dotfiles/bash/bashrc ~/.bashrc
-# vim
-ln -s .dotfiles/vim/vimrc ~/.vimrc
-# tmux + tmux plugin manager
-ln -s .dotfiles/tmux/tmux.conf ~/.tmux.conf
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-# Alacritty teriminal
-ln -s .dotfiles/terminal/alacritty.yml ~/.alacritty.yml
-# inputrc
-ln -s .dotfiles/inputrc ~/.inputrc
+mkdir ~/.config
 
-# Setup neovim
-mkdir -p ~/.config
-cd !$
-ln -s ../.dotfiles/nvim
+## initial symlinks
+ln -s .dotfiles/bash/bash_profile .bash_profile 
+ln -s .dotfiles/bash/bashrc .bashrc
+ln -s .dotfiles/inputrc .inputrc
+ln -s .dotfiles/tmux/tmux.conf .tmux.conf
+
+## miniforge > mambaforge >> conda
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
+bash Miniforge3-Linux-x86_64.sh
+# Then
+vim ~/.bashrc ~/.dotfiles/conda/setup
+# and comment out or delete the auto-generated conda stuff from the bottom of ~/.bashrc, moving it over
+# to ~/.dotfiles/conda/setup into the appropriate area if necessary.
+# Finally:
+ln -s .dotfiles/conda/condarc .condarc
+
+## Starship prompt
+# Just download a raw binary from https://github.com/starship/starship/releases, and install it in ~/bin/ 
+ln -s ~/.dotfiles/starship/starship.toml .config/starship.toml
+
+## vim & tmux
+mamba install vim tmux
+# neovim
+See ~/.dotfiles/astronvim-v3/README.setup.txt
+# tmux + tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+## Alacritty teriminal
+ln -s .dotfiles/terminal/alacritty.yml ~/.alacritty.yml
 
 # Currently my symlinks look like this:
 
 .bash_profile -> .dotfiles/bash/bash_profile
 .bashrc -> .dotfiles/bash/bashrc
-.nvimrc -> .vimrc
+.inputrc -> /Users/sydes/.dotfiles/inputrc
 .tmux.conf -> .dotfiles/tmux/tmux.conf
-.vimrc -> .dotfiles/vim/vimrc
