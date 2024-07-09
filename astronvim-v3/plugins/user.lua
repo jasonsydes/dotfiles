5d9f5dd3140534a4117710ab4c1788de5c4aa09d
Jason Sydes <sydes@uoregon.edu>
2024-07-09 13:45:30 -0700

    Reorganization. Partial support for ZSH.
    Mostly still sticking with BASH for now.
    
    More thoughtful approach to interactive/non-interactive shell.

--- astronvim-v3/plugins/user.lua
+++ astronvim-v3/plugins/user.lua
@@ -70,4 +70,134 @@ return {
 
   -- Disable these plugins that AstroNvim includes by default.
   {'goolord/alpha-nvim', enabled = false },
+  -- {'kevinhwang91/nvim-ufo', enabled = false },
+
+
+  -- {
+  --   "epwalsh/obsidian.nvim",
+  --
+  --   -- the obsidian vault in this default config  ~/obsidian-vault
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand':
+  --   -- event = { "bufreadpre " .. vim.fn.expand "~" .. "/my-vault/**.md" },
+  --   event = { "BufReadPre  */obsidian-vault/*.md" },
+  --   keys = {
+  --     {
+  --       "gf",
+  --       function()
+  --         if require("obsidian").util.cursor_on_markdown_link() then
+  --           return "<cmd>ObsidianFollowLink<CR>"
+  --         else
+  --           return "gf"
+  --         end
+  --       end,
+  --       noremap = false,
+  --       expr = true,
+  --     },
+  --     {
+  --       "<leader>;",
+  --       function()
+  --         return require("obsidian").util.toggle_checkbox()
+  --       end,
+  --       buffer = true,
+  --     },
+  --
+  --   },
+  --
+  --   ui = {
+  --     enable = true,  -- set to false to disable all additional syntax features
+  --     update_debounce = 200,  -- update delay after a text change (in milliseconds)
+  --     -- Define how various check-boxes are displayed
+  --     checkboxes = {
+  --       -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+  --       [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+  --       ["x"] = { char = "", hl_group = "ObsidianDone" },
+  --       [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+  --       ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+  --       -- Replace the above with this if you don't have a patched font:
+  --       -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+  --       -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+  --
+  --       -- You can also add more custom ones...
+  --     },
+  --     -- Use bullet marks for non-checkbox lists.
+  --     bullets = { char = "•", hl_group = "ObsidianBullet" },
+  --     external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+  --     -- Replace the above with this if you don't have a patched font:
+  --     -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+  --     reference_text = { hl_group = "ObsidianRefText" },
+  --     highlight_text = { hl_group = "ObsidianHighlightText" },
+  --     tags = { hl_group = "ObsidianTag" },
+  --     hl_groups = {
+  --       -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
+  --       ObsidianTodo = { bold = true, fg = "#f78c6c" },
+  --       ObsidianDone = { bold = true, fg = "#89ddff" },
+  --       ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
+  --       ObsidianTilde = { bold = true, fg = "#ff5370" },
+  --       ObsidianBullet = { bold = true, fg = "#89ddff" },
+  --       ObsidianRefText = { underline = true, fg = "#c792ea" },
+  --       ObsidianExtLinkIcon = { fg = "#c792ea" },
+  --       ObsidianTag = { italic = true, fg = "#89ddff" },
+  --       ObsidianHighlightText = { bg = "#75662e" },
+  --     },
+  --   },
+  --
+  --
+  --   dependencies = {
+  --     "nvim-telescope/telescope.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "hrsh7th/nvim-cmp",
+  --   },
+  --   opts = {
+  --     dir = vim.env.HOME .. "/obsidian-vault", -- specify the vault location. no need to call 'vim.fn.expand' here
+  --     use_advanced_uri = true,
+  --     finder = "telescope.nvim",
+  --     mappings = {
+  --
+  --
+  --     },
+  --
+  --     templates = {
+  --       subdir = "templates",
+  --       date_format = "%Y-%m-%d-%a",
+  --       time_format = "%H:%M",
+  --     },
+  --
+  --     -- turn on syntax highlighting...
+  --     highlight = { enable = true, },
+  --     opt = {
+  --       conceallevel=2,
+  --     },
+  --
+  --
+  --     workspaces = {
+  --       {
+  --         name = "chefs-log",
+  --         path = "~/ObsidianSync/v3/cooking/chefs-log",
+  --       },
+  --       {
+  --         name = "bgmp",
+  --         path = "~/ObsidianSync/v3/work/BGMP",
+  --       },
+  --     },
+  --
+  --     note_frontmatter_func = function(note)
+  --       -- This is equivalent to the default frontmatter function.
+  --       local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+  --       -- `note.metadata` contains any manually added fields in the frontmatter.
+  --       -- So here we just make sure those fields are kept in the frontmatter.
+  --       if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
+  --         for k, v in pairs(note.metadata) do
+  --           out[k] = v
+  --         end
+  --       end
+  --       return out
+  --     end,
+  --
+  --     -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
+  --     -- URL it will be ignored but you can customize this behavior here.
+  --     follow_url_func = vim.ui.open or require("astronvim.utils").system_open,
+  --   },
+  -- }
+
+
 }
